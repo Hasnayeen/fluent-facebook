279325ca61768c274cd0251da981e0038798f1fc
nhasnayeen <nehal.hasnayeen@gagagugu.com>
2016-08-06 20:02:02 +0600

    Initial setup

---
+++ src/providers/FluentFacobookServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Hasnayeen\FluentFacebook\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class FluentFacobookServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
